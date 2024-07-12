451521fa877364956d2e83ae4342bedd5fd49644
itiaditi <aditi.tlgt244@gmail.com>
2024-07-12 15:03:09 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,18 @@ app.post('/requests', async (req, res) => {
     const { url } = req.body;
     const result = [];
 
-    const browser = await puppeteer.launch();
+     const browser = await puppeteer.launch({
+        args: [
+          "--disable-setuid-sandbox",
+          "--no-sandbox",
+          "--single-process",
+          "--no-zygote",
+        ],
+        executablePath:
+          process.env.NODE_ENV === "production"
+            ? process.env.PUPPETEER_EXECUTABLE_PATH
+            : puppeteer.executablePath(),
+      });
     const page = await browser.newPage();
 
     await page.setRequestInterception(true);
